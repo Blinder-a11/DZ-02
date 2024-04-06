e33b2fe0417702c16d27c75c2d5e75a1f120d6db
Blinder-a11 <61007181+Blinder-a11@users.noreply.github.com>
2024-04-06 10:32:04 +0300

    Update Main.py
    Update file

--- Main.py
+++ Main.py
@@ -7,6 +7,9 @@ random.shuffle(lst)
 
 data = pd.DataFrame({'whoAmI': lst})
 
-one_hot_encoded = pd.get_dummies(data, columns=['whoAmI'])
-
-print(one_hot_encoded.head())
+data['tmp'] = 1
+data.set_index([data.index, 'whoAmI'], inplace=True)
+data = data.unstack(level=-1, fill_value = 0).astype(int)
+data.columns = data.columns.droplevel()
+data.columns.name = None
+print(data)
